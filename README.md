70f946eb0bba76dd79f6eab095613f3a4c79859a
SandActor <luka.timohin@yandex.ru>
2025-06-02 16:46:50 +0500

    1 шаг

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SandActor/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SandActor/frontend-project-11/actions)
-[![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=SandActor_frontend-project-46&metric=sqale_rating)](https://sonarcloud.io/summary/new_code?id=SandActor_frontend-project-46)
+[![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=SandActor_frontend-project-11&metric=sqale_rating)](https://sonarcloud.io/summary/new_code?id=SandActor_frontend-project-11)
+[![Vercel](https://vercel.com/button)](https://frontend-project-11.vercel.app)
--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import pluginReact from 'eslint-plugin-react'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
-      },
-    },
-  },
-  {
-    files: ['**/*.{jsx}'],
-    ...pluginReact.configs.recommended,
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-]
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
