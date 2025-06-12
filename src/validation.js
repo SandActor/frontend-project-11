b309ad09db753bd11ac0fe25c157533920d8b756
SandActor <luka.timohin@yandex.ru>
2025-06-12 14:15:41 +0500

    1 правка

--- src/validation.js
+++ src/validation.js
@@ -1,6 +1,15 @@
 import * as yup from 'yup'
 import i18n from './i18n'
 
+yup.setLocale({
+  mixed: {
+    required: () => i18n.t('form.errors.required'),
+  },
+  string: {
+    url: () => i18n.t('form.errors.url'),
+  }
+})
+
 const createSchema = (existingUrls) => {
   return yup.object().shape({
     url: yup
