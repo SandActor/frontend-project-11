a10810d7f137d88e4e30aa36709c463be9c32205
SandActor <luka.timohin@yandex.ru>
2025-06-19 15:20:12 +0500

    4 правка 5 тест

--- src/rssParser.js
+++ src/rssParser.js
@@ -1,26 +1,31 @@
 export const parseRSS = (xmlString) => {
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(xmlString, 'text/xml')
+  try {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(xmlString, 'text/xml')
 
-  const channel = doc.querySelector('channel')
-  const feed = {
-    title: channel.querySelector('title').textContent,
-    description: channel.querySelector('description').textContent,
-    link: channel.querySelector('link').textContent,
-    lastBuildDate: channel.querySelector('lastBuildDate')?.textContent,
-    generator: channel.querySelector('generator')?.textContent,
+    const channel = doc.querySelector('channel')
+    const feed = {
+      title: channel.querySelector('title').textContent,
+      description: channel.querySelector('description').textContent,
+      link: channel.querySelector('link').textContent,
+      lastBuildDate: channel.querySelector('lastBuildDate')?.textContent,
+      generator: channel.querySelector('generator')?.textContent,
+    }
+
+    const items = Array.from(doc.querySelectorAll('item')).map(item => ({
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+      pubDate: item.querySelector('pubDate')?.textContent,
+      creator: item.querySelector('dc\\:creator')?.textContent || 'Unknown author',
+      guid: item.querySelector('guid')?.textContent,
+    }))
+
+    return { feed, posts: items }
+  } 
+  catch {
+    throw new Error('Ресурс не содержит валидный RSS')
   }
-
-  const items = Array.from(doc.querySelectorAll('item')).map(item => ({
-    title: item.querySelector('title').textContent,
-    description: item.querySelector('description').textContent,
-    link: item.querySelector('link').textContent,
-    pubDate: item.querySelector('pubDate')?.textContent,
-    creator: item.querySelector('dc\\:creator')?.textContent || 'Unknown author',
-    guid: item.querySelector('guid')?.textContent,
-  }))
-
-  return { feed, posts: items }
 }
 
 export const getRSS = (url) => {
@@ -37,10 +42,6 @@ export const getRSS = (url) => {
       return parseRSS(data.contents)
     })
     .catch((error) => {
-      let errorRespons = error.message
-      if(error.message === "Cannot read properties of null (reading 'querySelector')") {
-        errorRespons = 'Ресурс не содержит валидный RSS'
-      }
-      throw new Error(errorRespons)
+      throw new Error(error.message)
     })
 }
