02006875f1da64745aa92ee88a54d3eadd357760
SandActor <luka.timohin@yandex.ru>
2025-06-19 15:28:36 +0500

    4 правка 5 тест

--- src/rssParser.js
+++ src/rssParser.js
@@ -1,41 +1,33 @@
 export const parseRSS = (xmlString) => {
-  try {
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(xmlString, 'text/xml')
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(xmlString, 'text/xml')
 
-    const channel = doc.querySelector('channel')
-    const feed = {
-      title: channel.querySelector('title').textContent,
-      description: channel.querySelector('description').textContent,
-      link: channel.querySelector('link').textContent,
-      lastBuildDate: channel.querySelector('lastBuildDate')?.textContent,
-      generator: channel.querySelector('generator')?.textContent,
-    }
-
-    const items = Array.from(doc.querySelectorAll('item')).map(item => ({
-      title: item.querySelector('title').textContent,
-      description: item.querySelector('description').textContent,
-      link: item.querySelector('link').textContent,
-      pubDate: item.querySelector('pubDate')?.textContent,
-      creator: item.querySelector('dc\\:creator')?.textContent || 'Unknown author',
-      guid: item.querySelector('guid')?.textContent,
-    }))
-
-    return { feed, posts: items }
-  } 
-  catch {
-    throw new Error('Ресурс не содержит валидный RSS')
+  const channel = doc.querySelector('channel')
+  const feed = {
+    title: channel.querySelector('title').textContent,
+    description: channel.querySelector('description').textContent,
+    link: channel.querySelector('link').textContent,
+    lastBuildDate: channel.querySelector('lastBuildDate')?.textContent,
+    generator: channel.querySelector('generator')?.textContent,
   }
+
+  const items = Array.from(doc.querySelectorAll('item')).map(item => ({
+    title: item.querySelector('title').textContent,
+    description: item.querySelector('description').textContent,
+    link: item.querySelector('link').textContent,
+    pubDate: item.querySelector('pubDate')?.textContent,
+    creator: item.querySelector('dc\\:creator')?.textContent || 'Unknown author',
+    guid: item.querySelector('guid')?.textContent,
+  }))
+
+  return { feed, posts: items }
 }
 
 export const getRSS = (url) => {
-  const proxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`
+  const proxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true12312`
 
   return fetch(proxyUrl)
     .then((response) => {
-      if (!response.ok) {
-        throw new Error('Ошибка сети')
-      }
       return response.json()
     })
     .then((data) => {
@@ -43,6 +35,6 @@ export const getRSS = (url) => {
     })
     .catch((error) => {
       console.log(error.message)
-      throw new Error(error.message)
+      throw new Error(error.message.includes('valid') ? error.message : 'Ресурс не содержит валидный RSS')
     })
 }
